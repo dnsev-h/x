852eee8b492afa59713be8f59d2ef89255406bf4
dnsev-h <dnsev-h@users.noreply.github.com>
2019-05-14 19:34:29 -0400

    Create navigation bar module

---
+++ src/api/navigation-bar.js
@@ -0,0 +1,24 @@
+"use strict";
+
+
+function addLink(label, url, order) {
+	const n = document.getElementById("nb");
+	if (n === null) { return; }
+
+	const div = document.createElement("div");
+	const a = document.createElement("a");
+	a.href = url;
+	a.textContent = label;
+	if (typeof(order) === "number") {
+		div.style.order = `${order}`;
+	}
+	div.appendChild(a);
+	n.appendChild(div);
+
+	return div;
+}
+
+
+module.exports = {
+	addLink
+};
