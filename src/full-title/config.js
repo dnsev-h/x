a4ba62e14e230212bef4415b412cd642a66d56d2
dnsev-h <dnsev-h@users.noreply.github.com>
2019-05-14 19:36:19 -0400

    Add support for different display modes
    This also includes the addition of the settings page.

---
+++ src/full-title/config.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const configKey = "x-full-title-config";
+const configDefault = {
+	displayMode: "hover" // "none", "hover", "always"
+};
+
+module.exports = require("../config").create(configKey, configDefault);
--- src/full-title/main.js
+++ src/full-title/main.js
@@ -2,14 +2,53 @@
 
 const ready = require("../ready");
 const style = require("../style");
+const config = require("./config");
+const pageType = require("../api/page-type");
+const settings = require("../api/settings");
+
+
+async function initializeSettings() {
+	settings.initialize();
+
+	const section = settings.addSection("Full Title", "full-title", 0);
+	if (section !== null) {
+		await setupSettings(section);
+	}
+}
+
+async function setupSettings(container) {
+	container.innerHTML = require("./settings.html");
+	bindInput(container, "displayMode");
+}
+
+function bindInput(container, settingName) {
+	const n = container.querySelector(`[data-x-settings-for=${settingName}]`);
+	if (n === null) { return null; }
+
+	config.bindInput(container.querySelector(`[data-x-settings-for=${settingName}]`), settingName);
+}
+
+async function initializeMode() {
+	const c = await config.get();
+	document.documentElement.setAttribute("data-x-option-full-title-display-mode", c.displayMode);
+}
 
 
 function main() {
+	settings.addLink();
+
+	const currentPageType = pageType.get(document, location);
+	if (currentPageType === "settings") {
+		initializeSettings();
+	}
+
 	const id = "x-full-title";
 	if (!style.hasStylesheet(id)) {
 		const src = require("./style.css");
 		style.addStylesheet(src, id);
 	}
+
+	initializeMode();
 }
 
 
