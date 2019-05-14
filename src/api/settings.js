761d53cc976401db0f0f09ac19cd797948f4a57f
dnsev-h <dnsev-h@users.noreply.github.com>
2019-05-14 19:34:48 -0400

    Create module for script settings

---
+++ src/api/settings.js
@@ -0,0 +1,115 @@
+"use strict";
+
+
+const style = require("../style");
+const urlFragment = require("../url-fragment");
+
+
+const settingsContainerClass = "x-settings-container";
+const settingsContainerHiddenClass = "x-settings-container-hidden";
+const defaultSettingsHiddenClass = "x-settings-hidden";
+
+let settingsContainerOuter = null;
+let settingsContainer = null;
+
+
+function addLink() {
+	const id = "x-nav-settings-link";
+
+	let n = document.getElementById(id);
+	if (n !== null) { return n; }
+
+	const navBar = require("./navigation-bar");
+	n = navBar.addLink("x", `/uconfig.php${urlFragment.create("settings")}`, 1);
+	n.id = id;
+	return n;
+}
+
+function initialize() {
+	settingsContainerOuter = document.querySelector("#outer.stuffbox");
+	if (settingsContainerOuter === null) { return; }
+
+	settingsContainer = settingsContainerOuter.querySelector(`.${settingsContainerClass}`);
+	if (settingsContainer === null) {
+		settingsContainer = document.createElement("div");
+		settingsContainer.className = `${settingsContainerClass} ${settingsContainerHiddenClass}`;
+		settingsContainerOuter.appendChild(settingsContainer);
+	}
+
+	const id = "x-settings";
+	if (!style.hasStylesheet(id)) {
+		const src = require("./style/settings.css");
+		style.addStylesheet(src, id);
+	}
+
+	urlFragment.addRoute(/^\/settings(\/[\w\W]*)?$/, onSettingsPageChanged);
+}
+
+function onSettingsPageChanged(match) {
+	setSettingsVisible(match !== null);
+}
+
+function setSettingsVisible(visible) {
+	if (settingsContainerOuter === null || settingsContainer === null) { return; }
+
+	if (settingsContainer.classList.contains(settingsContainerHiddenClass) !== visible) {
+		// No change
+		return;
+	}
+
+	settingsContainer.classList.toggle(settingsContainerHiddenClass, !visible);
+
+	for (const child of settingsContainerOuter.children) {
+		if (child === settingsContainer) { continue; }
+		child.classList.toggle(defaultSettingsHiddenClass, visible);
+	}
+}
+
+function addSection(header, id, order) {
+	if (settingsContainer === null) { return null; }
+
+	const fullId = `x-settings-section-${id}`;
+
+	let section = settingsContainer.querySelector(`#${fullId}`);
+	if (section === null) {
+		section = document.createElement("div");
+		section.id = fullId;
+		section.className = "x-settings-section";
+		if (typeof(order) === "number") {
+			section.style.order = `${order}`;
+		}
+
+		settingsContainer.appendChild(section);
+	}
+
+	let cls = "x-settings-section-header";
+	let sectionHeader = section.querySelector(`.${cls}`);
+	if (sectionHeader === null) {
+		sectionHeader = document.createElement("h2");
+		sectionHeader.className = cls;
+		sectionHeader.textContent = header;
+		const relative = section.firstChild;
+		if (relative !== null) {
+			section.insertBefore(relative, sectionHeader);
+		} else {
+			section.appendChild(sectionHeader);
+		}
+	}
+
+	cls = "x-settings-section-content";
+	let sectionContent = section.querySelector(`.${cls}`);
+	if (sectionContent === null) {
+		sectionContent = document.createElement("div");
+		sectionContent.className = cls;
+		section.appendChild(sectionContent);
+	}
+
+	return sectionContent;
+}
+
+
+module.exports = {
+	addLink,
+	initialize,
+	addSection
+};
