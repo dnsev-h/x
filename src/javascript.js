c4112dc09f5d68fdf6cbbca7a93dc9b447146715
dnsev-h <dnsev-h@users.noreply.github.com>
2019-05-18 18:25:22 -0400

    Add utility class for injecting javascript

---
+++ src/javascript.js
@@ -0,0 +1,23 @@
+"use strict";
+
+
+function inject(func, args) {
+	const parent = document.body || document.documentElement.querySelector("head") || document.documentElement || null;
+	if (!parent) { return false; }
+
+	const scriptNode = document.createElement("script");
+	const argStr = (Array.isArray(args) && args.length > 0) ?
+		`...${JSON.stringify(args, null, "")}` :
+		"";
+	scriptNode.textContent = `(${func.toString()})(${argStr});`;
+
+	parent.appendChild(scriptNode);
+	parent.removeChild(scriptNode);
+
+	return true;
+}
+
+
+module.exports = {
+	inject
+};
