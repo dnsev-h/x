b8b3b0fcee0c30e991c947472f9b5e8c55c42a5a
dnsev-h <dnsev-h@users.noreply.github.com>
2019-05-18 18:30:57 -0400

    Use shared API for gallery link creation

--- src/gallery-metadata/main.js
+++ src/gallery-metadata/main.js
@@ -7,60 +7,45 @@ let downloadDataUrl = null;
 
 
 function setupDownloadLink() {
-	const isDark = require("../style").isDark();
+	const galleryLinks = require("../api/gallery-links");
+	const link = galleryLinks.createRightSideLink("Metadata JSON").link;
+	if (link === null) { return; }
 
-	const parent = document.querySelector("#gd5");
-	if (parent === null) { return; }
-
-	const linkP = document.createElement("p");
-	linkP.className = "g2 gsp";
-
-	const img = document.createElement("img");
-	img.src = (isDark ? "https://exhentai.org/img/mr.gif" : "https://ehgt.org/g/mr.gif");
-	linkP.appendChild(img);
-
-	linkP.appendChild(document.createTextNode(" "));
-
-	const link = document.createElement("a");
-	link.href = "#"
-	link.textContent = "Metadata JSON";
 	link.setAttribute("download", "info.json");
-	linkP.appendChild(link);
+	link.href = "#";
 
 	link.addEventListener("click", onDownloadLinkClicked, false);
 	link.addEventListener("auxclick", onDownloadLinkClicked, false);
-
-	parent.appendChild(linkP);
 }
 
-function createDownloadDataUrl(link) {
-	const getGalleryInfo = require("../api/gallery-info/get-from-html");
-	let info = null;
+function getGalleryInfo() {
+	const getFromHtml = require("../api/gallery-info/get-from-html");
 	try {
-		info = getGalleryInfo(document);
+		return getFromHtml(document);
 	} catch (e) {
 		console.error(e);
+		return null;
 	}
+}
+
+function createDownloadDataUrl(info) {
 	if (info === null) { return null; }
 
 	const infoString = JSON.stringify(info.toCommonJson(), null, "  ");
 	const blob = new Blob([ infoString ], { type: "application/json" });
-	const url = URL.createObjectURL(blob);
-
-	link.setAttribute("href", url);
-
-	return url;
+	return URL.createObjectURL(blob);
 }
 
 function onDownloadLinkClicked(e) {
 	if (downloadDataUrl === null) {
-		downloadDataUrl = createDownloadDataUrl(this);
+		downloadDataUrl = createDownloadDataUrl(getGalleryInfo());
 		if (downloadDataUrl === null) {
 			console.error("Failed to create download data");
 			e.preventDefault();
 			e.stopPropagation();
 			return false;
 		}
+		link.setAttribute("href", downloadDataUrl);
 	}
 }
 
