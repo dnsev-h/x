663e800cb0aa27c3a00de5fc030eb64bbe63daab
dnsev-h <dnsev-h@users.noreply.github.com>
2019-05-18 18:26:52 -0400

    Add method of fetching gallery info from API

---
+++ src/api/gallery-info/fetch.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const fetch = require("../../fetch");
+const getFromJson = require("./get-from-json");
+
+
+function setArrayCount(array, count, fill) {
+	const ii = array.length;
+	if (ii !== count) {
+		if (ii > count) {
+			array.splice(count, ii - count);
+		} else {
+			for (let i = ii; i < count; ++i) {
+				array.push(fill);
+			}
+		}
+	}
+}
+
+async function getGalleryInfo(identifiers) {
+	const gidList = [];
+	const isArray = Array.isArray(identifiers);
+	if (isArray) {
+		for (const identifier of identifiers) {
+			gidList.push([ identifier.id, identifier.token ]);
+		}
+	} else {
+		gidList.push([ identifiers.id, identifiers.token ]);
+	}
+
+	const data = {
+		method: "gdata",
+		gidlist: gidList
+	};
+
+	const fetchResult = await fetch.post({
+		gm: true,
+		url: "/api.php",
+		data: JSON.stringify(data)
+	});
+
+	const resultJson = JSON.parse(fetchResult.responseText);
+
+	const results = [];
+	for (const json of resultJson.gmetadata) {
+		if (json.error) {
+			results.push(null);
+		} else {
+			const info = getFromJson(json);
+			results.push(info);
+		}
+	}
+
+	setArrayCount(results, gidList.length, null);
+
+	return isArray ? results : results[0];
+}
+
+
+module.exports = {
+	get: getGalleryInfo
+};
