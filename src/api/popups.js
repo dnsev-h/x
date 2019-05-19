75dd8d774a187ec637bbe083cd5cec2dda68006e
dnsev-h <dnsev-h@users.noreply.github.com>
2019-05-18 23:54:11 -0400

    Add module for spawning popups

---
+++ src/api/popups.js
@@ -0,0 +1,29 @@
+"use strict";
+
+function showOnClick(selector, url, size) {
+	let width = 675;
+	let height = 415;
+	if (size !== null && typeof(size) === "object") {
+		({width, height} = size);
+	}
+
+	require("../javascript").inject((selector, url, width, height) => {
+		document.querySelector(selector).addEventListener("click", (e) => {
+			e.preventDefault();
+			e.stopPropagation();
+			try {
+				return popUp(url, width, height);
+			} catch (e) {
+				return false;
+			}
+		}, false);
+	}, [ selector, url, width, height ]);
+}
+
+
+module.exports = {
+	showOnClick,
+	sizes: {
+		favorites: { width: 675, height: 415 }
+	}
+};
