ea92a5a3b53e1f384cfc71c7c28e1aff37413650
dnsev-h <dnsev-h@users.noreply.github.com>
2019-05-18 23:53:48 -0400

    Add module for generating common URLs

---
+++ src/api/urls.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function uploader(usernName) {
+	return `/uploader/${usernName}`;
+}
+
+function category(category) {
+	return `/${category}`;
+}
+
+function favoritesPopup(id, token) {
+	return `/gallerypopups.php?gid=${id}&t=${token}&act=addfav`;
+}
+
+
+module.exports = {
+	uploader,
+	category,
+	favoritesPopup
+};
