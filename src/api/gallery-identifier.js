050e7fc62bb438f224e65d98f7ce038b1c6405b2
dnsev-h <dnsev-h@users.noreply.github.com>
2019-05-18 23:54:32 -0400

    Move GalleryIdentifier to its own file

---
+++ src/api/gallery-identifier.js
@@ -0,0 +1,24 @@
+"use strict";
+
+class GalleryIdentifier {
+	constructor(id, token) {
+		this.id = id;
+		this.token = token;
+	}
+
+	static createFromUrl(url) {
+		const match = /^.*?\/\/.+?\/(.*?)(\?.*?)?(#.*?)?$/.exec(url);
+		if (match === null) { return null; }
+
+		const path = match[1].replace(/^\/+|\/+$/g, "").replace(/\/{2,}/g, "/").split("/");
+		if (path[0] !== "g" || path.length < 3) { return null; }
+
+		const id = parseInt(path[1], 10);
+		return (Number.isNaN(id) ? null : new GalleryIdentifier(id, path[2]));
+	}
+}
+
+
+module.exports = {
+	GalleryIdentifier
+};
