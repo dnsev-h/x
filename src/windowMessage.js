5d58fc16c9363dc339f6e8abe0947f10b44e295b
dnsev-h <dnsev-h@users.noreply.github.com>
2019-05-20 20:52:20 -0400

    Add communication class for handling window messages

---
+++ src/windowMessage.js
@@ -0,0 +1,41 @@
+"use strict";
+
+let commands = null;
+
+
+function registerCommand(commandName, callback) {
+	if (commands === null) {
+		commands = {};
+		window.addEventListener("message", onWindowMessage, false);
+	}
+
+	commands[commandName] = callback;
+}
+
+function post(targetWindow, commandName, data) {
+	targetWindow.postMessage({
+		xData: { command: commandName, data: data }
+	}, window.location.origin);
+}
+
+function onWindowMessage(e) {
+	if (e.origin !== window.origin) { return; }
+
+	let data = e.data;
+	if (data === null || typeof(data) !== "object") { return; }
+
+	data = data.xData;
+	if (data === null || typeof(data) !== "object") { return; }
+	if (typeof(data.command) !== "string") { return; }
+
+	const callback = commands[data.command];
+	if (typeof(callback) !== "function") { return; }
+
+	callback(e, data.data);
+}
+
+
+module.exports = {
+	registerCommand,
+	post
+};
