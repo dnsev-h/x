2e4a82a347e8230dcf7ed7f02a804d058353ec41
dnsev-h <dnsev-h@users.noreply.github.com>
2019-05-25 12:19:53 -0400

    Add more options for binding inputs to config values

--- src/config.js
+++ src/config.js
@@ -37,12 +37,28 @@ function create(configKey, configDefault) {
 		}
 	}
 
-	async function bindInput(node, settingName) {
+	async function bindInput(node, settingName, options, valueName) {
 		const c = await get();
-		node.value = c[settingName];
+
+		if (typeof(valueName) === "undefined") {
+			valueName = getDefaultValueName(node);
+		}
+
+		const updateInput = () => {
+			const {value, valid} = convertToType(c[settingName], options, true);
+			if (valid) { node[valueName] = value; }
+		};
+
+		updateInput();
+
 		node.addEventListener("change", () => {
-			c[settingName] = node.value;
-			save();
+			const {value, valid} = convertToType(node[valueName], options, false);
+			if (valid) {
+				c[settingName] = value;
+				save();
+			}
+
+			updateInput();
 		}, false);
 	}
 
@@ -52,6 +68,78 @@ function create(configKey, configDefault) {
 		save,
 		bindInput
 	};
+}
+
+
+const defaultTypeConvertOptions = {};
+
+
+function getDefaultValueName(node) {
+	switch (node.tagName) {
+		case "INPUT":
+			if (node.type === "checkbox") { return "checked"; }
+			break;
+	}
+
+	return "value";
+}
+
+function convertToType(value, options, toInput) {
+	if (typeof(options) === "string") {
+		return convertToTypeNormalized(value, options, defaultTypeConvertOptions, toInput);
+	} if (options !== null && typeof(options) === "object" && typeof(options.type) === "string") {
+		return convertToTypeNormalized(value, options.type, options, toInput);
+	} else {
+		return { value, valid: true };
+	}
+}
+
+function convertToTypeNormalized(value, typeName, options, toInput) {
+	let valid = true;
+
+	// Convert
+	switch (typeName) {
+		case "boolean":
+			value = !!value;
+			break;
+		case "integer":
+		case "number":
+			value = (typeName === "number" ? parseFloat(`${value}`) : parseInt(`${value}`, 10));
+			if (!Number.isFinite(value)) {
+				value = 0;
+				valid = false;
+			}
+			break;
+		case "string":
+			value = `${value}`;
+			break;
+	}
+
+	// Transform
+	if (!toInput && typeof(options.inputToValue) === "function") {
+		value = options.inputToValue(value);
+	}
+
+	// Limits
+	switch (typeName) {
+		case "integer":
+		case "number":
+			if (typeof(options.min) === "number" && value < options.min) { value = options.min; }
+			if (typeof(options.max) === "number" && value > options.max) { value = options.max; }
+			break;
+		case "string":
+			if (typeof(options.maxLength) === "number" && value.length > options.maxLength) {
+				value = value.substr(0, options.maxLength);
+			}
+			break;
+	}
+
+	// Transform
+	if (toInput && typeof(options.valueToInput) === "function") {
+		value = options.valueToInput(value);
+	}
+
+	return { value, valid };
 }
 
 
