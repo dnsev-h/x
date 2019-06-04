6aaa1f00d6d7670f01c1c22b7d674b8a017734f4
dnsev-h <dnsev-h@users.noreply.github.com>
2019-06-04 19:19:32 -0400

    favorites v1.0.1

--- builds/x-favorites.meta.js
+++ builds/x-favorites.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        x/favorites
-// @version     1.0.0
+// @version     1.0.1
 // @author      dnsev-h
 // @namespace   dnsev-h
 // @description Improvements to adding favorites
--- changes/favorites.md
+++ changes/favorites.md
@@ -1,4 +1,9 @@
 # x/favorites changelog
 
+## v1.0.1
+* Minor API updates
+  * Page type detection
+  * JSHint cleanup
+
 ## v1.0.0
 * Initial release
